fc54c2142f803d2e90d9ba4a49c7613a6592892c
Willians <williansvf@hotmail.com>
2022-10-19 14:27:22 -0300

    config ngnix

--- desafioCompose/app/index.js
+++ desafioCompose/app/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 3000
 const config = {
    host: 'db',
@@ -11,24 +12,24 @@ const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
 const initDB = () => {
-   
-   const sqlCreatetable = `CREATE TABLE if not exists people(id int not null auto_increment, nome varchar(255), primary key(id))`
+   const sqlCreatetable = `CREATE TABLE if not exists people(id int not null auto_increment, name varchar(255), primary key(id))`
    connection.query(sqlCreatetable)
-  
 }
 
 app.post('/',(req,res)=>{
-   const {nome} = req.body
-   const sqlInsertPeople = `INSERT INTO people(nome) values(${nome})`
+   const {name} = req.body
+   const sqlInsertPeople = `INSERT INTO people(name) values('${name}')`
    connection.query(sqlInsertPeople)
+   res.status(201).send()
 })
 
 app.get('/', (req, res) => {
    const sqlSelect = `SELECT * FROM people`;
+   console.log(req.query)
    connection.query(sqlSelect,(err, result)=>{
       if(!err){
          res.send(`<h1>Full Cycle Rocks!</h1>
-         ${result.map(people=>people.nome).join("<BR>")}`)
+         ${result.map(people=>people.name).join("<BR>")}`)
       }
 
    })
